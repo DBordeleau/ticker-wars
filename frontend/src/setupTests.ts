61d723ffb6cb4b8ae8e31dc4b026407c91d7757b
Dillon Bordeleau <dillonbordeleau@gmail.com>
2026-07-04 11:34:37 -0400

    Added missing jsdom browser shims in setupTests.ts

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -14,4 +14,41 @@ class ResizeObserverMock {
   }
 }
 
+class IntersectionObserverMock {
+  root = null;
+  rootMargin = "";
+  thresholds = [];
+
+  observe() {
+    return undefined;
+  }
+
+  unobserve() {
+    return undefined;
+  }
+
+  disconnect() {
+    return undefined;
+  }
+
+  takeRecords() {
+    return [];
+  }
+}
+
 global.ResizeObserver = ResizeObserverMock;
+global.IntersectionObserver = IntersectionObserverMock;
+window.scrollTo = jest.fn();
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: (query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  }),
+});
